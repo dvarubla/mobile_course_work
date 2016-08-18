008826017ef75b5c1e694195395a6b6110ab6fdd
dvarubla <dvarubla@users.noreply.github.com>
2016-08-19 00:21:55 +0700

    Меню с кнопками, которые используют одну и ту же строку, которая форматируется в коде activity

--- app/src/main/java/study/courseproject/ListTasksActivity.java
+++ app/src/main/java/study/courseproject/ListTasksActivity.java
@@ -1,6 +1,9 @@
 package study.courseproject;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.Button;
+import java.lang.String;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +16,14 @@ public class ListTasksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_tasks);
+
+        int[] ids={R.id.task1, R.id.task2, R.id.task3};
+        Object g=findViewById(R.id.task1);
+        for(int i=0; i<ids.length; i++){
+            Button bt=(Button)findViewById(ids[i]);
+            bt.setText(String.format(bt.getText().toString(), i+1));
+        }
+
     }
 
 }
